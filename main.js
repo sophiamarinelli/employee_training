e6ba021378808fb92eb8c7012bf0155e10ca8703
sophiamarinelli <31392946+sophiamarinelli@users.noreply.github.com>
2018-03-22 21:06:06 -0600

    hover fixed

--- main.js
+++ main.js
@@ -5,38 +5,8 @@ $( document ).ready(function() {
 	var incorrect = 0;
 
     $(".blank1").mouseenter(function(){
-    	$(".blank1_options").addClass(".show");
+    	$(".blank1_options").addClass("show");
         console.log("1")
-    });
-
-    $(".blank1_options").click(function(){
-    	var $chosenOption = $(".op1").html();
-    	$(".blank1").html($chosenOption);
-    	$(".optionbox").removeClass(".show");
-    	correct++;
-    });
-
-    $(".blank1_options").click(function(){
-        var $chosenOption2 = $(".op2").html();
-        $(".blank1").html($chosenOption2);
-        $(".optionbox").removeClass(".show");
-        correct++;
-    });
-
-    $(".blank1").mouseenter( function(){
-        $(".blank1_options").addClass(".show");
-    });
-
-    $(".op1").click(function(){
-        var $chosenOption = $(".op1").html();
-        $(".blank1").html($chosenOption);
-        $(".blank1_options").removeClass(".show");
-        correct++;
-    });
-
-    $(".submit-button").click(function(){
-    	$(".correct-count").html(correct);
-    	$(".incorrect-count").html(incorrect);
     });
 
 });
